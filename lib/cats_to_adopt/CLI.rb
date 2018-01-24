9579262da1b454326d63390b0496965fc0f733d3
Allyson Wesman <allysonwesman@gmail.com>
2018-01-23 16:46:02 -0800

    add ability for user to list cats and exit program

--- lib/cats_to_adopt/CLI.rb
+++ lib/cats_to_adopt/CLI.rb
@@ -2,12 +2,14 @@
 class CatsToAdopt::CLI
 
   def call
-    puts "Cats available now:"
     list_cats
     menu
+    goodbye
   end
 
   def list_cats
+    puts "\nCats available now:"
+
     puts <<~HEREDOC
     1. Finn - Domestic Short Hair - Male - Young - 1 mile away
     2. Cliff - Domestic Short Hair - Male - Kitten - 1 mile away
@@ -17,17 +19,23 @@ class CatsToAdopt::CLI
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the cat you would like more info on."
+      puts "\nEnter the number of the cat you would like more info on."
       puts "Type list to list all cats or exit to exit the program."
       input = gets.strip
 
       if input.to_i > 0
-        puts "Here is the cat info you requested!"
+        puts "\nHere is the cat info you requested!"
       elsif input == "list"
         list_cats
+      elsif input == "exit"
+        puts "\nClosing program ..."
       else
         puts "Please enter the number of a cat, list, or exit."
       end
+    end
+
+    def goodbye
+      puts "\nGoodbye!"
     end
 
   end
