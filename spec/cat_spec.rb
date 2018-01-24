324d88b030e2363ac715828afff975ab5eec7266
Allyson Wesman <allysonwesman@gmail.com>
2018-01-24 09:58:22 -0800

    add new test files

---
+++ spec/cat_spec.rb
@@ -0,0 +1,26 @@
+describe "Cat" do
+
+  describe "#all" do
+    it "returns an array of all the cats" do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#scrape_cats" do
+    it 'correctly scrapes cat info for the cats' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#print_cat_info" do
+    it 'correctly scrapes cat info for the cats' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#list_cats" do
+    it 'lists info for all the cats' do
+      expect(true).to eq(false)
+    end
+  end
+end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,30 @@
+describe "CLI" do
+
+  describe "::new" do
+    it "has basic attributes" do
+      expect(true).to eq(false)
+    end
+
+    it "has extra attributes" do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#menu" do
+    it 'correctly scrapes cat info for the cats' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#goodbye" do
+    it 'correctly scrapes cat info for the cats' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe "#list_cats" do
+    it 'correctly scrapes cat info for the cats' do
+      expect(true).to eq(false)
+    end
+  end
+end
