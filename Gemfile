ec44403d54c1335f05f78d86ba3357e600284c6c
Allyson Wesman <allysonwesman@gmail.com>
2018-01-23 16:33:17 -0800

    fix gemspec file

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-git_source(:github) {|cats-to-adopt-cli-gem| "https://github.com/#{cats-to-adopt-cli-gem}" }
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in cats_to_adopt.gemspec
 gemspec
--- lib/cats_to_adopt/CLI.rb
+++ lib/cats_to_adopt/CLI.rb
@@ -4,6 +4,7 @@ class CatsToAdopt::CLI
   def call
     puts "Cats available now:"
     list_cats
+    menu
   end
 
   def list_cats
@@ -11,6 +12,10 @@ class CatsToAdopt::CLI
     1. Finn - Domestic Short Hair - Male - Young - 1 mile away
     2. Cliff - Domestic Short Hair - Male - Kitten - 1 mile away
     HEREDOC
+  end
+
+  def menu
+    puts "Enter the number of the cat you would like more info on."
   end
 
 end
